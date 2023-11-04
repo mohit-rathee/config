9a7930c9ba5538702504cbf24f99511ead627c38
mohit-rathee <mohit.rathee2505@gmail.com>
2023-11-04 23:04:32 +0530

    saving current config

---
+++ after/plugin/colors.lua
@@ -0,0 +1,10 @@
+function ColorNeovim(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = 'none' })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = 'none' })
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = 'none' });
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = 'none' });
+end
+
+ColorNeovim()
